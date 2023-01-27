9d557141be61d82503c5ab043a9d05597f975450
mohamad makkawi <mohamad.makkawi@outlook.com>
2023-01-27 19:21:11 +0300

    Add project files.

---
+++ SingleResponsibilityPrinciple/After/Services/LocationService.cs
@@ -0,0 +1,15 @@
+﻿using SingleResponsibilityPrinciple.After.Models;
+
+namespace SingleResponsibilityPrinciple.After.Services;
+
+internal class LocationService
+{
+    public static void Add(Tree tree, string location)
+    {
+        tree.Location = location;
+    }
+    public static void Remove(Tree tree)
+    {
+         tree.Location = string.Empty;
+    }
+}
